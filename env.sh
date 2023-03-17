a1c82ceed697847ae42ff4313046a039a908c3c6
Markus Bader <markus.bader@mx-robotics.com>
2023-03-17 15:52:06 +0100

    minor

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 # rudi
 export ROS_DISTRO=humble
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///cyclonedds.xml 
+export CYCLONEDDS_URI=file:///$(pwd)/cyclonedds.xml 
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 echo "** ROS2 $ROS_DISTRO initialized with $RMW_IMPLEMENTATION**"
